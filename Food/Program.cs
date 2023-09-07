82ae820caa634be6fa405287415cd15c9ac5c00e
Classonsy <Classonsy@CLASSONSY>
2023-09-07 21:03:35 +0300

    Добавьте файлы проекта.

---
+++ Food/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            int food;
+            int FoodUnitPrice = 10;
+            bool isAbleToPay;
+            Console.WriteLine($"Добро пожаловать в пекарню! Сегодня еда по {FoodUnitPrice} монет ");
+            Console.WriteLine("Сколько у вас золота?");
+                money =  Convert.ToInt32(Console.ReadLine());
+            Console.Write("Выберите колличество еды:");
+            food = Convert.ToInt32(Console.ReadLine());
+
+            isAbleToPay = money >= food * FoodUnitPrice;
+            food *= Convert.ToInt32(isAbleToPay);
+
+            money -= food * FoodUnitPrice;
+            Console.WriteLine($"У вас в сумке {food} единиц еды, и {money} монет.");
+        }
+    }
+}
